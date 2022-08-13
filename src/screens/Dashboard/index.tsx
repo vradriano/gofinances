1eb0db1ed7d0b98b7440b285d7c1c02cf3602546
unknown <vradriano@hotmail.com>
2022-08-13 17:21:37 -0300

    highlightcard component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HightlightCard } from '../../components/HighlightCard';
 import { 
   Container,
   Header,
@@ -32,6 +33,8 @@ export function Dashboard() {
         </UserWrapper>
 
       </Header>
+
+      <HightlightCard />
     </Container>
   )
 }
