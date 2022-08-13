3b36f18f9dd64a62392204a271b99774034eb9db
unknown <vradriano@hotmail.com>
2022-08-12 22:42:01 -0300

    global styles

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
 import { Dashboard } from './src/screens/Dashboard';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/global/styles/theme'
 
 export default function App() {
-  return <Dashboard />
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,12 +1,14 @@
-import styled from "styled-components"; "styled-components/native";
+import styled from "styled-components/native";
 
-export const Container = styled.view`
+export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #999;
+  background-color: ${({ theme }) => theme.colors.primary};
 `;
 
-export const Title = styled.text`
-  font-size: '26px';
+export const Title = styled.Text`
+  font-size: '24px';
+  font-weight: bold;
+  color: ${({ theme}) => theme.colors.title}
 `
