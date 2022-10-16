5940ef2e715cda7a54975ce8cfb5484ef6e64c81
unknown <vradriano@hotmail.com>
2022-10-16 17:25:12 -0300

    load login error

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -32,7 +32,6 @@ export function SignIn() {
     } catch (error) {
       console.log(error)
       Alert.alert('Não foi possivel conectar a conta Google')
-    } finally {
       setIsLoading(false)
     }
   }
@@ -44,9 +43,8 @@ export function SignIn() {
     } catch (error) {
       console.log(error)
       Alert.alert('Não foi possivel conectar a conta Apple')
-    } finally {
       setIsLoading(false)
-    }
+    } 
   }
 
   return (
